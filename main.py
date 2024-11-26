b3436f63a5eb33329b05a597c7bd26a3ea66c60e
79930 <likrakstrimy@gmail.ru>
2024-11-26 23:58:32 +0700

    Добавлена кнопка для открытия карты через браузер

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
+import webbrowser
 
 
 def get_coordinates(city, key):
@@ -7,30 +8,42 @@ def get_coordinates(city, key):
         geocoder = OpenCageGeocode(key)
         results = geocoder.geocode(city, language='ru')
         if results:
-            lat=round(results[0]["geometry"]["lat"],3)
-            lng=round(results[0]["geometry"]["lng"],3)
+            lat=round(results[0]["geometry"]["lat"],2)
+            lng=round(results[0]["geometry"]["lng"],2)
             country = results[0]['components']['country']
+            osm_url = f'https://www.openstreetmap.org/?mlat={lat}&mlon={lng}'
             if 'state' in results[0]['components']:
                 region = results[0]['components']['state']
-                return f"Широта:{lat}, Долгота:{lng}\n Страна: {country}. Регион: {region}"
+                return {
+                    'coordinates': f"Широта:{lat}, Долгота:{lng}\n Страна: {country}. Регион: {region}",
+                    'map_url': osm_url
+                }
             else:
-                return f"Широта:{lat}, Долгота:{lng}\n Страна: {country}."
-            return f"Широта:{lat}, Долгота:{lng}\n Страна: {country}. Регион: {region}"
+                return {
+                    'coordinates': f"Широта:{lat}, Долгота:{lng}\n Страна: {country}.",
+                    'map_url': osm_url
+                }
         else:
             return "Данные не найдены"
     except Exception as err:
         print(f"Неизвестная ошибка {err}")
 
 
-def show_coordinates(event=None):
+def show_coordinates(_=None):
+    global map_url
     city = entry.get()
-    coordinates = get_coordinates(city, key)
-    label.config(text=f"Координаты города {city}:\n {coordinates}")
+    result = get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}:\n {result['coordinates']}")
+    map_url = result['map_url']
 
+
+def show_map():
+    if map_url:
+        webbrowser.open(map_url)
 
 
 key = 'aea38c6f1db14c1da396e3ab10e1f936'
-
+map_url = ''
 
 
 win = Tk()
@@ -43,6 +56,9 @@ entry.bind('<Return>', show_coordinates)
 
 button = Button(text='Поиск координат', command=show_coordinates)
 button.pack()
+
+map_button = Button(text='Показать карту', command=show_map)
+map_button.pack()
 
 label = Label(text='Введите город и нажмите на кнопку')
 label.pack()
