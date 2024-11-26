c1769b63c898b83b3f61877ffa46447c3c5bc71f
79930 <likrakstrimy@gmail.ru>
2024-11-27 00:27:46 +0700

    Добавлена кнопка для Очистки метки с результатом и валюта государства указанного города

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ def get_coordinates(city, key):
             lat=round(results[0]["geometry"]["lat"],2)
             lng=round(results[0]["geometry"]["lng"],2)
             country = results[0]['components']['country']
+            currency = results[0]['annotations']['currency']['name']
             osm_url = f'https://www.openstreetmap.org/?mlat={lat}&mlon={lng}'
             if 'state' in results[0]['components']:
                 region = results[0]['components']['state']
                 return {
-                    'coordinates': f"Широта:{lat}, Долгота:{lng}\n Страна: {country}. Регион: {region}",
+                    'coordinates': f"Широта:{lat}, Долгота:{lng}\n Страна: {country}. Регион: {region}. Валюта: {currency}",
                     'map_url': osm_url
                 }
             else:
                 return {
-                    'coordinates': f"Широта:{lat}, Долгота:{lng}\n Страна: {country}.",
+                    'coordinates': f"Широта:{lat}, Долгота:{lng}\n Страна: {country}.\n Валюта: {currency}.",
                     'map_url': osm_url
                 }
         else:
@@ -42,13 +43,18 @@ def show_map():
         webbrowser.open(map_url)
 
 
+def clear():
+    entry.delete(0, END)
+    label.destroy()
+
+
 key = 'aea38c6f1db14c1da396e3ab10e1f936'
 map_url = ''
 
 
 win = Tk()
 win.title('Координаты городов')
-win.geometry('300x100')
+win.geometry('300x200')
 
 entry = Entry()
 entry.pack()
@@ -57,10 +63,13 @@ entry.bind('<Return>', show_coordinates)
 button = Button(text='Поиск координат', command=show_coordinates)
 button.pack()
 
+label = Label(text='Введите город и нажмите на кнопку')
+label.pack()
+
 map_button = Button(text='Показать карту', command=show_map)
 map_button.pack()
 
-label = Label(text='Введите город и нажмите на кнопку')
-label.pack()
+del_button = Button(text='Очистить', command=clear)
+del_button.pack()
 
 win.mainloop()
